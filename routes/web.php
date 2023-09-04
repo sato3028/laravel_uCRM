1c36035ebaf70aeb551cbcd1e717e83c7d4fa69a
sato3028 <sakayusho007@gmail.com>
2023-09-05 00:00:52 +0900

    顧客検索

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PurchaseController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,6 +23,9 @@ Route::resource('items', ItemController::class)
 ->middleware(['auth', 'verified']);
 
 Route::resource('customers', CustomerController::class)
+->middleware(['auth', 'verified']);
+
+Route::resource('purchases', PurchaseController::class)
 ->middleware(['auth', 'verified']);
 
 Route::get('/', function () {
